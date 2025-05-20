71d94ce902a2ffc36496bde83301310c32178c95
Jeffrey Inyang <jeffrey.inyang@yahoo.com>
2025-05-20 21:22:37 +0100

    Update Footer.jsx
    Alr then

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer(){
+  return(
+    <>
+    <h3 style={{ color: '#7B7878'}} >@JobHive 2025. All rights reserved</h3>
+    </>
+    );
+}
+
+export default Footer;
